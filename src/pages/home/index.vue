86c88d41697024668cc9547c81e353abd41c1449
Semen Ishinov <ectoplazm@gmail.com>
2023-06-06 09:14:27 +0300

    First commit

---
+++ src/pages/home/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template lang="pug">
+div(class="text-3xl font-bold underline") Hello Home
+</template>
---
+++ src/shared/ui/Template.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// interface Props {}
+
+// const props = defineProps<Props>()
+
+// const emit = defineEmits<{
+//   (e: 'onOpenClick', item: TreeItem): void
+// }>()
+</script>
+
+<template lang="pug">
+div Template vue
+</template>
+<style scoped lang="scss">
+</style>
